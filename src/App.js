07f8c306b2eb5c775d9f9078fec3e16e0fe38170
moon-seon-yeong <moonsy@biztechi.com>
2023-03-16 12:32:03 +0900

    First Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+function Counter(props) {
+  console.log('props',props);
+  return <div>
+    <h1>Counter</h1>
+    <button>➕</button> 👉 0
+  </div>
+}
+function App() {
+  return (
+    <div>
+      <Counter title="불면증 카운터" initValue="10"></Counter>
+      <Counter title="입장객 카운터" initValue="20"></Counter>
+    </div>
+  );
+}
+
+export default App;
